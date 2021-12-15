fe87b2fd3ab3fa4e67e67bf0305ec5efbb2ddfca
Zack Dev <zackdev@Akays-MacBook-Air.local>
2021-12-15 17:45:50 -0600

    setting up Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,38 @@
 import Portfolio from "./Portfolio"
 import Contacts from "./Contact"
 import Resume from "./Resume"
+import Home from "./Home"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 const Navbar = () => {
     return (
+    <div>
+    <Router>
         <div>
-               {/* <nav className="navbar navbar-">
-        <ul>
-            <li><a href="./html/aboutMe.html">About Me</a> </li>
-       
-            <li><a  href="./html/contactMe.html">Contact Me</a></li>
-       
-            <li><a href="./html/porfolio.html">portfolio</a></li>
-               
-            <li ><a  href="./html/resume.html">Resume</a> </li>
-           </ul>
-                  
-              
-           </nav> 
-    */}
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href={Portfolio}>Projects</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href={Resume}>Resume</a>
-      </li>
-      <li className="nav-item">
-        <a class="nav-link" href={Contacts}>Contacts</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+         <ul>
+         <li>
+         <Link to="/">Home</Link>
+         </li>
+         <li>
+         <Link to="/Resume">Resume</Link>
+         </li> 
+         <li>
+         <Link to="/Portfolio">Projects</Link>
+         </li>
+         <li>
+         <Link to="/Contact">Contacts</Link>
+         </li>
+         </ul>
+
+         <hr />
+
+         <Route exact path="/" component={Home} />
+         <Route path="/Resume" component={Resume} />
+         <Route path="/Portfolio" component={Portfolio} />
+         <Route path="/Contact" component={Contacts} />
         </div>
+     </Router> 
+     </div>
     )
 }
 
 export default Navbar
-
